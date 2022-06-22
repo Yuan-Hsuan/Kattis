4b2a18d793d78e295cc1af238a9d139be45c47cc
Yuan-Hsuan <70123070+Yuan-Hsuan@users.noreply.github.com>
2022-06-22 14:28:52 +0800

    Update beavergnaw.cpp

--- beavergnaw.cpp
+++ beavergnaw.cpp
@@ -9,18 +9,18 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
-	float D, V;
+	double D, V;
     while(cin >> D && cin >> V) {
 
 		if( D == 0 && V == 0 ){
 			break;
 		}
 
-        float shape = ( pow(D/2, 2) * D * 2 / 3 * M_PI - V ) * 1.5;
+        double shape = ( pow(D/2, 2) * D * 2 / 3 * M_PI - V ) * 1.5;
 
-        float r = pow((shape / (2 * M_PI)), (1.0/3));
+        double r = pow((shape / (2 * M_PI)), (1.0/3));
 
-        cout << r * 2 << endl;
+        cout << fixed << setprecision(9) << r * 2 << endl;
     }
     
     return 0;
